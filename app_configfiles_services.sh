4814f9a8d6a6f78ecd7120bcad5a0897ccdc0c53
josemarquezmontoro <94985283+josemarquezmontoro@users.noreply.github.com>
2022-03-07 20:25:37 +0100

    Update app_configfiles_services.sh

--- app_configfiles_services.sh
+++ app_configfiles_services.sh
@@ -4,7 +4,6 @@ sed -i 's/#CPU_SCALING_GOVERNOR_ON_AC=powersave/CPU_SCALING_GOVERNOR_ON_AC=perfo
 sed -i 's/#CPU_SCALING_GOVERNOR_ON_BAT=powersave/CPU_SCALING_GOVERNOR_ON_BAT=powersave/g' /etc/tlp.conf
 systemctl enable tlp
 systemctl start tlp
-pacman -Syu libgtop lm_sensors gnome-icon-theme-symbolic
 pamac install vim
 pamac install visual-studio-code-bin
 pamac install eclipse-java
@@ -15,12 +14,10 @@ systemctl start virtnetworkd
 pamac install chromium
 pamac install microsoft-edge-stable-bin
 pamac install thunderbird
-pamac install spotify
 pamac install discord
 pamac install libreoffice
 pamac install gimp
 pamac install ventoy-bin
-pamac install balena-etcher
 pamac install lutris
 pamac install retroarch
 pamac install cpupower-gui
