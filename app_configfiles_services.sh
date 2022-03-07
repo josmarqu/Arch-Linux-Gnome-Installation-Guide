251caabde442e0447150c7060882aa204ba3eafc
josemarquezmontoro <94985283+josemarquezmontoro@users.noreply.github.com>
2022-03-07 20:16:26 +0100

    Update app_configfiles_services.sh
    Code error fixed and cpu-power-gui added

--- app_configfiles_services.sh
+++ app_configfiles_services.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 pamac install tlp
-sed -i 's/#CPU_SCALING_GOVERNOR_ON_AC=powersave/CPU_SCALING_GOVERNOR_ON_AC=performance/g'
-sed -i 's/#CPU_SCALING_GOVERNOR_ON_BAT=powersave/CPU_SCALING_GOVERNOR_ON_BAT=powersave/g'
+sed -i 's/#CPU_SCALING_GOVERNOR_ON_AC=powersave/CPU_SCALING_GOVERNOR_ON_AC=performance/g' /etc/tlp.conf
+sed -i 's/#CPU_SCALING_GOVERNOR_ON_BAT=powersave/CPU_SCALING_GOVERNOR_ON_BAT=powersave/g' /etc/tlp.conf
 systemctl enable tlp
 systemctl start tlp
 pacman -Syu libgtop lm_sensors gnome-icon-theme-symbolic
@@ -13,13 +13,14 @@ pamac install qemu
 systemctl enable virtnetworkd
 systemctl start virtnetworkd
 pamac install chromium
-pamac install edge
+pamac install microsoft-edge-stable-bin
 pamac install thunderbird
 pamac install spotify
 pamac install discord
 pamac install libreoffice
 pamac install gimp
-pamac install ventoy
+pamac install ventoy-bin
 pamac install balena-etcher
 pamac install lutris
 pamac install retroarch
+pamac install cpupower-gui
