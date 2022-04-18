61d21b0ea482bb448ed72815de274f4bbbc0279e
josemarquezmontoro <94985283+josemarquezmontoro@users.noreply.github.com>
2022-04-18 16:38:22 +0200

    Update yay_pamac_gnome.sh

--- scripts/yay_pamac_gnome.sh
+++ scripts/yay_pamac_gnome.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 sudo pacman -S base-devel
-sudo pacman -S git
 cd /var/cache/pacman/pkg 
 sudo mkdir /var/cache/pacman/pkg/yay
-sudo chmod 777 /var/cache/pacman/pkg/yay
-git clone https://aur.archlinux.org/yay.git
+sudo git clone https://aur.archlinux.org/yay.git
+user=`whoami`
+sudo chown -R $user:users /var/cache/pacman/pkg/yay
 cd /var/cache/pacman/pkg/yay
 makepkg -si
 yay -Syy pamac-aur-git
